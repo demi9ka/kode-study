1b7d946cc087a6522c952c46d1b4e0b93cdb9d1b
demi9ka <demi9ka@gmail.com>
2025-04-08 13:42:29 +0200

    init commit

---
+++ src/app/app.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import { Button, SafeAreaView } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { AppNavigation } from '@routing/app-navigation'
+
+import { Providers } from './providers'
+import { Storybook } from '../../.storybook'
+
+export const App = () => {
+  const [isStorybookVisible, setIsStorybookVisible] = useState(true)
+
+  if (isStorybookVisible) {
+    return (
+      <Providers>
+        <Storybook />
+        <SafeAreaView>
+          <Button
+            onPress={() => setIsStorybookVisible(false)}
+            title='Перейти к приложению'
+          />
+        </SafeAreaView>
+      </Providers>
+    )
+  }
+
+  return (
+    <Providers>
+      <NavigationContainer>
+        <AppNavigation />
+      </NavigationContainer>
+    </Providers>
+  )
+}
---
+++ src/routing/app-navigation.tsx
@@ -0,0 +1,5 @@
+import { HomeScreen } from '@screens/home'
+
+export const AppNavigation = () => {
+  return <HomeScreen />
+}
