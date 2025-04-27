76f6792aa2f6b1a34730b5307d9a7ba693696767
demi9ka <demi9ka@gmail.com>
2025-04-27 14:20:05 +0200

    [+] add payment

--- src/screens/payment/payment-create/payment-create-connector.tsx
+++ src/screens/payment/payment-create/payment-create-connector.tsx
@@ -1,11 +1,22 @@
 import { RootStackParamsList } from '@app/navigation/navigators/root-navigator'
 import { StackScreenProps } from '@react-navigation/stack'
 import { PaymentCreate } from './payment-create'
-import { useEffect, useState } from 'react'
 import { balance } from './constansts'
 import { usePaymentService } from '@entities/payments/hooks/use-payment-service'
-import { getMaskedPhone, numberValueFormMoney, moneyString } from './model'
+import {
+  getMaskedPhone,
+  numberValueFormMoney,
+  moneyString,
+  TPaymentCreateForm,
+  moneyOnCard,
+  getSchema,
+  onlyDigitsValue,
+} from './model'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { formatBalance } from './helpers'
+import { FieldErrors, FormProvider, useForm } from 'react-hook-form'
+import { useEffect } from 'react'
+import { addToast } from '@features/toast'
 
 export type PaymentCreateProps = StackScreenProps<
   RootStackParamsList,
@@ -17,38 +28,82 @@ export const PaymentCreateConnector = ({
   route,
 }: PaymentCreateProps) => {
   const { id, icon, name } = route.params
-  const [phone, setPhone] = useState('')
-  const [amount, setAmount] = useState(0)
   const { data } = usePaymentService(id)
   const { recipient_mask, cashback_percentage } = !data
     ? { cashback_percentage: 0 }
     : data.data
 
-  const handleChangePhone = (inputText: string) =>
-    setPhone(
-      getMaskedPhone({
-        phone: inputText,
-        pattern: recipient_mask
-          ? '+7 ' + recipient_mask.replaceAll('X', '9')
-          : undefined,
-      }),
-    )
+  const form = useForm<TPaymentCreateForm>({
+    mode: 'onSubmit',
+    reValidateMode: 'onChange',
+    defaultValues: {
+      moneyOnCard,
+      phone: '',
+      money: moneyString(0),
+    },
+    resolver: zodResolver(getSchema()),
+  })
 
-  const handleChangeAmount = (inputValue: string) => {
-    const numberValue = numberValueFormMoney(inputValue)
-    setAmount(numberValue)
-  }
+  const { control, watch, setValue, handleSubmit } = form
 
-  const continueTransaction = () => {
+  const moneyValue = watch('money')
+  const phoneValue = watch('phone')
+
+  //   const handleChangePhone = (inputText: string) =>
+  //     setPhone(
+  //       getMaskedPhone({
+  //         phone: inputText,
+  //         pattern: recipient_mask
+  //           ? '+7 ' + recipient_mask.replaceAll('X', '9')
+  //           : undefined,
+  //       }),
+  //     )
+
+  //   const handleChangeAmount = (inputValue: string) => {
+  //     const numberValue = numberValueFormMoney(inputValue)
+  //     setAmount(numberValue)
+  //   }
+
+  const onValid = ({ money, phone }: TPaymentCreateForm) => {
     navigation.navigate('paymentConfirm', {
-      amount,
+      amount: numberValueFormMoney(money),
       phone,
       name,
       cashback_percentage: cashback_percentage!,
     })
   }
 
-  const currentMoneyValue = numberValueFormMoney(String(amount))
+  const onInvalid = (errors?: FieldErrors<TPaymentCreateForm>) => {
+    if (errors?.phone?.message) {
+      addToast({
+        message: errors?.phone?.message,
+        variant: 'error',
+      })
+      return
+    }
+    if (errors?.money?.message) {
+      addToast({
+        message: errors?.money?.message,
+        variant: 'error',
+      })
+    }
+  }
+  const onChipPress = (value: number) => {
+    setValue('money', moneyString(value))
+  }
+
+  useEffect(() => {
+    const newPhoneValue = getMaskedPhone({ phone: phoneValue })
+    setValue('phone', newPhoneValue)
+  }, [phoneValue])
+
+  useEffect(() => {
+    const numberValue = numberValueFormMoney(moneyValue)
+    const stringValue = moneyString(numberValue)
+    setValue('money', stringValue)
+  }, [moneyValue])
+
+  const currentMoneyValue = numberValueFormMoney(String(moneyValue))
   const cacheBackString =
     currentMoneyValue > 0 && cashback_percentage
       ? `Ваш кешбек составит ${cashback_percentage / 100}% - ${moneyString(
@@ -56,20 +111,18 @@ export const PaymentCreateConnector = ({
         )}`
       : ''
 
-  //
-
   const balanceFormatted = formatBalance(balance)
 
   return (
-    <PaymentCreate
-      balance={balanceFormatted}
-      continueTransaction={continueTransaction}
-      handleChangeAmount={handleChangeAmount}
-      handleChangePhone={handleChangePhone}
-      iamgeSource={icon}
-      phone={phone}
-      cacheBackString={cacheBackString}
-      value={amount}
-    />
+    <FormProvider {...form}>
+      <PaymentCreate
+        moneyOnCard={balanceFormatted}
+        handleSubmit={handleSubmit(onValid, onInvalid)}
+        iamgeSource={icon}
+        control={control}
+        cacheBackString={cacheBackString}
+        onChipPress={onChipPress}
+      />
+    </FormProvider>
   )
 }
--- src/shared/ui/atoms/input/input.tsx
+++ src/shared/ui/atoms/input/input.tsx
@@ -7,11 +7,9 @@ import React, {
 } from 'react-native'
 import { useTheme, styled } from '@shared/ui/theme'
 import { IconClose } from '@shared/ui/icons'
-import { ReactNode } from 'react'
+import { forwardRef, ReactNode } from 'react'
 
-const Wrapper = styled(View)<{
-  //хз почему но padding по вертикали Больше чем нужно, не знаю как поправить
-}>`
+const Wrapper = styled(View)`
   flex-direction: row;
   padding: ${({ theme }) => theme.spacing(3 / 4)}px
     ${({ theme }) => theme.spacing(1)}px
@@ -22,41 +20,68 @@ const Wrapper = styled(View)<{
   border-radius: ${({ theme }) => theme.spacing(1)}px;
   gap: ${({ theme }) => theme.spacing(1 / 2)}px;
 `
-const InputNode = styled(TextInput)`
-  color: ${({ theme }) => theme.palette.text.primary};
-  font-size: 15px;
+const InputNode = styled(TextInput)<{
+  $textColor: string
+}>`
+  color: ${({ $textColor }) => $textColor};
   flex-grow: 2;
+  font-family: ${({ theme }) => theme.typography.body15Regular.fontFamily};
+  font-size: ${({ theme }) => theme.typography.body15Regular.size};
+  letter-spacing: ${({ theme }) =>
+    theme.typography.body15Regular.letterSpacing};
+  line-height: ${({ theme }) => theme.typography.body15Regular.lineHeight};
 `
 
-export interface Props extends TextInputProps {
+export type Props = {
   hasClearButton?: boolean
+  isError: boolean
   leftSection?: ReactNode
   wrapperStyle?: ViewProps['style']
-}
+  onChange?: TextInputProps['onChangeText']
+  onNativeChange?: TextInputProps['onChange']
+} & Omit<TextInputProps, 'onChange' | 'onChangeText'>
 
-export const Input = ({
-  hasClearButton,
-  leftSection,
-  wrapperStyle,
-  ...props
-}: Props) => {
-  const theme = useTheme()
-  const handleOnPress = () => props.onChangeText && props.onChangeText('')
-  return (
-    <Wrapper style={wrapperStyle}>
-      {leftSection}
+export const Input = forwardRef<TextInput, Props>(
+  (
+    {
+      isError,
+      hasClearButton,
+      leftSection,
+      wrapperStyle,
+      onNativeChange,
+      onChange,
+      ...props
+    },
+    ref,
+  ) => {
+    const theme = useTheme()
+    const handleOnPress = () => onChange && onChange('')
+    return (
+      <Wrapper style={wrapperStyle}>
+        {leftSection}
 
-      <InputNode
-        placeholderTextColor={theme.palette.text.tertiary}
-        keyboardAppearance='dark'
-        {...props}
-      />
+        <InputNode
+          ref={ref}
+          onChangeText={onChange}
+          onChange={onNativeChange}
+          keyboardAppearance='dark'
+          placeholderTextColor={
+            isError
+              ? theme.palette.indicator.error
+              : theme.palette.text.tertiary
+          }
+          $textColor={
+            isError ? theme.palette.indicator.error : theme.palette.text.primary
+          }
+          {...props}
+        />
 
-      {hasClearButton ? (
-        <TouchableOpacity onPress={handleOnPress}>
-          <IconClose color={theme.palette.content.tertiary} />
-        </TouchableOpacity>
-      ) : null}
-    </Wrapper>
-  )
-}
+        {hasClearButton ? (
+          <TouchableOpacity onPress={handleOnPress}>
+            <IconClose color={theme.palette.content.tertiary} />
+          </TouchableOpacity>
+        ) : null}
+      </Wrapper>
+    )
+  },
+)
